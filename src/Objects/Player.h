c8fed8bb402b3cb5c66020979bfeb6dc65ce3383
yutatanaka <koryotennis99@gmail.com>
2015-11-18 17:15:41 +0900

    プレイヤー実装

---
+++ src/Objects/Player.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+class Player {
+public:
+	Player();
+	//メンバー変数
+	int hp, atk;			//体力、攻撃力
+	float pos_x, pos_y;		//座標x, y 
+	float scale_x, scale_y;	//スケールx, y
+	float speed;			//スピード
+	float jumpPower;		//ジャンプ力
+	float gaugeBar;			//必殺技のゲージバー
+	bool isJump;			//ジャンプのフラグ
+	bool isDeathbrow;		//必殺技のフラグ
+	//メンバーメソッド
+	void update();
+	void draw();
+};
--- src/Scene/mainGame.cpp
+++ src/Scene/mainGame.cpp
@@ -11,13 +11,36 @@ es::MainGame::MainGame() :
 
 
 void es::MainGame::update() {
-  isFinish_ = env().isPushKey(KeyBind::Space);
-  auto hoge = env().isPushButton(Mouse::LEFT);
+	//isFinish_ = env().isPushKey(KeyBind::Space);
+	auto hoge = env().isPushButton(Mouse::LEFT);
+	//プレイヤー移動
+	player.pos_x += player.speed;
+	//落下加速度を加える
+	player.jumpPower -= 3.0f;
+	//もし地面についていたら止まる
+	if (player.pos_y < 0){
+		player.isJump = false;
+		player.pos_y = 0.0f;
+		player.jumpPower = 0.0f;
 
+	}
+	//ジャンプボタンを押していて、地面についていたらジャンプ
+	if ((env().isPushKey(KeyBind::Space)) && player.isJump == false){
+		player.isJump = true;
+		player.jumpPower = 40;
+	}
+
+	if (player.pos_x >= 100 && player.pos_x <= 300 && player.pos_y  >= 250){
+		player.pos_y = 250;
+		player.jumpPower = 0;
+	}
+	else
+		//落下処理
+		player.pos_y += player.jumpPower;
 }
 
-
 void es::MainGame::draw() {
-  eg::drawCircle(Vec2f::Zero(),
-                 Vec2f::Ones() * 20.0f, 20, Color::blue);
+	eg::drawCircle(Vec2f(player.pos_x,player.pos_y),Vec2f(player.scale_x,player.scale_y), 20, Color::blue);
+	//eg::drawBox(Vec2f(player.pos_x, player.pos_y), Vec2f(player.scale_x, player.scale_y), Color::green);
+	eg::drawBox(Vec2f(100, 150), Vec2f(200, 50), Color::green);
 }
